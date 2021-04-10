a627876e005d2bd0faf4ff3728374c061d013992
vastutsav <utsavbarman@gmail.com>
2021-04-10 18:04:59 +0530

    Create countSymbols.py
    FASTEST MODE
    
    The program:
    The nucleotides inside a DNA sequence can be represented by a string composed by A, C, G and T. A sample string representing a DNA sequence is "ATGCTTCAGAAAAGGTCAGCG".
    
    Your task is to count how many times the symbols A, C, G and T appear in the string s.
    
    INPUT:
    A single line, composed of A, C, G and T.
    
    OUTPUT:
    The number of times A, C, G and T appear in the string s, separated by a space.
    
    CONSTRAINTS:
    0 < len(s) < 1000
    
    EXAMPLE:
    Input
    AACT
    Output
    2 1 0 1

---
+++ countSymbols.py
@@ -0,0 +1,12 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+s = input()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print(s.count('A'), s.count('C'), s.count('G'), s.count('T'))
