9d900dcdb04706fefacd060737873a1b679253d5
vastutsav <utsavbarman@gmail.com>
2021-04-25 02:57:25 +0530

    Create geometricMean.py
    # Fastest Mode
    
    Calculate the geometric mean of inputs a and b. The geometric mean of two numbers is the square root of a * b.
    Input
    Line 1: An integer a
    Line 2: An integer b
    Output
    Line 1: An integer c, the geometric mean of a and b
    Constraints
    a > 0
    b > 0
    Example
    Input
    2
    8
    Output
    4

---
+++ geometricMean.py
@@ -0,0 +1,13 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+a = int(input())
+b = int(input())
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print(int((a*b)**0.5))
