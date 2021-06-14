5ca82f20fccd5ea5fd093b6464601c9615ba344c
vastutsav <utsavbarman@gmail.com>
2021-06-15 03:06:41 +0530

    Create reboundHeight.py
    A ball is dropped and bounces. At each rebound, the ball reaches half the height of the previous rebound.
    
    How high the ball must be dropped so it reaches exactly the height H after the sixth rebound?
    
    INPUT:
    An integer H, the height reached by the ball after the sixth rebound.
    
    OUTPUT:
    The height at which we must drop the ball.
    
    CONSTRAINTS:
    1 ≤ H ≤ 10000
    
    EXAMPLE:
    Input
    5
    Output
    320

---
+++ reboundHeight.py
@@ -0,0 +1,12 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+h = int(input())
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print(2**6 * h)
